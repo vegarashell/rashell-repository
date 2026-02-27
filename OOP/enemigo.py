14a7b9f96c92f31eda1559501ee4abe46f19eb17
vegarashell <alessandravega327@gmail.com>
2026-02-26 18:37:56 -0800

    se creo alzombie

--- OOP/enemigo.py
+++ OOP/enemigo.py
@@ -1,7 +1,21 @@
-class enemigo:
+class Enemigo:
     tipo_enemigo: str
-    puntos_energia: int=10
-    ataque=1
+    puntos_energia: int= 10
+    ataque= 1
 
-    def _init_(self,tipo_enemigo,puntos_energia=10, ataque=1):
-        self
+    def __init__(self,tipo_enemigo, puntos_energia=10, ataque=1):
+        self._tipo_enemigo = tipo_enemigo 
+        self.puntos_energia = puntos_energia 
+        self.ataque = ataque 
+
+def get_tipo_enemigo(self):
+    return self.__tipo_enemigo
+
+def habla (self):
+    print(f"Yo son {self._tipo_enemigo}. Preparado para pelear!!")
+
+    def camina(self):
+        print(f"{self._tipo_enemigo}se mueve cerca de ti!!")
+
+def atacar(self):
+    print(f"{self.__tipo_enemigo}ataca con un {self.ataque}de daño!!")
